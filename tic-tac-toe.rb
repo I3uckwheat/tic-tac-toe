9739b827d76c3e01fd68f8d46f28fae14d003590
Briggs Elsperger <briggs.elsperger@hotmail.com>
2017-09-14 20:21:56 -0500

    Adds comments and changes formatting

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -1,3 +1,4 @@
+# Creates player and assigns them either an x or an o
 class Player
   @@player_number = 0
   attr_accessor :name, :piece
@@ -14,18 +15,19 @@ class Player
   end
 end
 
+# Creates Board
 class Board
   attr_accessor :locations
   def initialize
     @locations = []
-    @locations = (0...9).map { |x| @locations[x] = " " }
+    @locations = (0...9).map { |x| @locations[x] = ' ' }
   end
 
   def display_board
     count = 0
     @locations.each_slice(3) do |x, y, z|
       printf "  #{x} | #{y} | #{z}\n"
-      puts " -----------" if count < 2
+      puts ' -----------' if count < 2
       count += 1
     end
   end
