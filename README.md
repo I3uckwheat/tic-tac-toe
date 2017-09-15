273028d6d950cd763d76dd4a755547a401d9ca5e
Briggs Elsperger <briggs.elsperger@hotmail.com>
2017-09-15 14:10:59 -0500

    Removes player class, refactor

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 This is the Tic Tac Toe project for The Odin Project
+
+Couch of tomato has good stuff about this
--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -1,26 +1,24 @@
 # Sets up the play field
 class Game
   def initialize(name1, name2)
-    @player1 = Player.new(name1, "x")
-    @player2 = Player.new(name2, "o")
+    @players = {player1: [name1, "x"], player2: [name2, "o"] }
     @board = Board.new
   end
 
-  # Creates player and assigns them either an x or an o
-  class Player
-    def initialize(name, sign)
-      @name = name
-      @sign = sign
-    end
+  def show_board
+    @board.display_board
+  end
 
-    def place(location); end
+  def place(location)
+    sign = "x"
+    @board.place_piece(location, sign)
   end
 
   # Creates Board
   class Board
     def initialize
       @locations = []
-      @locations = (0...9).map { |x| @locations[x] = ' ' }
+      @locations = (0...9).map { |x| @locations[x] = x }
     end
 
     def display_board
@@ -30,6 +28,10 @@ class Game
         puts ' -----------' if count < 2
         count += 1
       end
+    end
+
+    def place_piece(location, sign)
+      puts location, sign
     end
   end
 end
