e8a9d32f3d526846863bf8997227aef82ec6b234
Briggs Elsperger <briggs.elsperger@hotmail.com>
2017-09-15 13:13:00 -0500

    Refactor

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -1,34 +1,35 @@
-# Creates player and assigns them either an x or an o
-class Player
-  @@player_number = 0
-  attr_accessor :name, :piece
-
-  def initialize(name)
-    @@player_number += 1
-    @name = name
-
-    @piece = if @@player_number == 1
-               'x'
-             else
-               'o'
-             end
-  end
-end
-
-# Creates Board
-class Board
-  attr_accessor :locations
-  def initialize
-    @locations = []
-    @locations = (0...9).map { |x| @locations[x] = ' ' }
+# Sets up the play field
+class Game
+  def initialize(name1, name2)
+    @player1 = Player.new(name1, "x")
+    @player2 = Player.new(name2, "o")
+    @board = Board.new
   end
 
-  def display_board
-    count = 0
-    @locations.each_slice(3) do |x, y, z|
-      printf "  #{x} | #{y} | #{z}\n"
-      puts ' -----------' if count < 2
-      count += 1
+  # Creates player and assigns them either an x or an o
+  class Player
+    def initialize(name, sign)
+      @name = name
+      @sign = sign
+    end
+
+    def place(location); end
+  end
+
+  # Creates Board
+  class Board
+    def initialize
+      @locations = []
+      @locations = (0...9).map { |x| @locations[x] = ' ' }
+    end
+
+    def display_board
+      count = 0
+      @locations.each_slice(3) do |x, y, z|
+        printf "  #{x} | #{y} | #{z}\n"
+        puts ' -----------' if count < 2
+        count += 1
+      end
     end
   end
 end
